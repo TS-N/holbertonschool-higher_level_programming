fc38ebfbaaab2ce12901a136261a31ad093071a7
TS-N <2501@holbertonschool.com>
2020-12-08 17:01:56 +0000

    fix:13 add other edge cases

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
-  * insert_node - inserts a number into a sorted singly linked list
-  * @head: the address of the head of the list
-  * @number: the number to add into the list
-  * Return: a pointer to the newly created node or NULL
-  **/
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: the address of the head of the list
+ * @number: the number to add into the list
+ * Return: a pointer to the newly created node or NULL
+ **/
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t	*new;
 	listint_t	*node;
-	listint_t	*buf;
+	listint_t	*prev = 0;
 
 	if (!head)
 		return (0);
@@ -19,27 +19,33 @@ listint_t *insert_node(listint_t **head, int number)
 		return (0);
 	new->n = number;
 	new->next = 0;
+	node = *head;
 	if (!*head)
-		*head = new;
-	else
 	{
-		node = *head;
-		while (node)
-		{
-			if (!node->next)
-			{
-				node->next = new;
-				break;
-			}
-			else if (node->next->n > number)
-			{
-				buf = node->next;
-				node->next = new;
-				new->next = buf;
-				break;
-			}
-			node = node->next;
-		}
+		*head = new;
+		return (new);
 	}
+	while (node->next)
+	{
+		if (node->next->n > number)
+		{
+			prev = node->next;
+			node->next = new;
+			new->next = prev;
+			return (new);
+		}
+		prev = node;
+		node = node->next;
+	}
+	if (node->n > number)
+	{
+		new->next = node;
+		if (!prev)
+			*head = new;
+		else
+			prev->next = new;
+	}
+	else
+		node->next = new;
 	return (new);
 }
