0b7a085e7dfcd5d98efce851b6cc87d3b0ce10df
TS-N <2501@holbertonschool.com>
2020-12-10 12:15:00 +0000

    fix:2 add s at argument when many or 0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import sys
 l = len(sys.argv)
-print("{:d} argument{}".format(l - 1, "." if l == 1 else ":"))
+if l == 1:
+    s = "s."
+elif l == 2:
+    s =":"
+else:
+    s = "s:"
+print("{:d} argument{}".format(l - 1, s))
 for i in range(1, l):
     print("{}: {}".format(i, sys.argv[i]))
