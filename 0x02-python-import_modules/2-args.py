0134e33ad8f03981d079eaa3b96ebc76cacc69f8
TS-N <2501@holbertonschool.com>
2020-12-10 14:08:26 +0000

    fix:2 import guard

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-l = len(sys.argv)
-if l == 1:
-    s = "s."
-elif l == 2:
-    s =":"
-else:
-    s = "s:"
-print("{:d} argument{}".format(l - 1, s))
-for i in range(1, l):
-    print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    l = len(sys.argv)
+    if l == 1:
+        s = "s."
+    elif l == 2:
+        s =":"
+    else:
+        s = "s:"
+    print("{:d} argument{}".format(l - 1, s))
+    for i in range(1, l):
+        print("{}: {}".format(i, sys.argv[i]))
