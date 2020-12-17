4372660d388f683ee2a20ddd256f1ab31965cb56
TS-N <2501@holbertonschool.com>
2020-12-17 16:42:23 +0000

    fix:6 guard ex:7-10 create

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for n in sorted(a_dictionary):
-        print("{}: {}".format(n, a_dictionary[n]))
+    if a_dictionary:
+        for n in sorted(a_dictionary):
+            print("{}: {}".format(n, a_dictionary[n]))
