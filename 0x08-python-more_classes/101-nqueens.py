b874793881e3ac07775548b3536c0fbf6b92f85b
TS-N <2501@holbertonschool.com>
2021-01-11 17:59:34 +0000

    fix:101 exit status

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -54,15 +54,15 @@ def main():
     """ main function """
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        return 1
+        sys.exit(1)
     try:
         N = int(sys.argv[1])
     except:
         print("N must be a number")
-        return 1
+        sys.exit(1)
     if N < 4:
         print("N must be at least 4")
-        return 1
+        sys.exit(1)
     solve(N)
 
 main()
