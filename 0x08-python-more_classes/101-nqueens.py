32000a48878cbb4c55377bd5745d903908d37387
TS-N <2501@holbertonschool.com>
2021-01-11 22:23:42 +0000

    checker test

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -54,15 +54,15 @@ def main():
     """ main function """
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
+        exit(1)
     try:
         N = int(sys.argv[1])
     except:
         print("N must be a number")
-        sys.exit(1)
+        exit(1)
     if N < 4:
         print("N must be at least 4")
-        sys.exit(1)
+        exit(1)
     solve(N)
 
 if __name__ == "__main__":
