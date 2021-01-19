0602870e36dc2aab4a8373e09009bea73458cc40
TS-N <2501@holbertonschool.com>
2021-01-19 19:41:39 +0000

    fix:4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ def inherits_from(obj, a_class):
     instance of a class that inherited
     (directly or indirectly) from the specified class
     otherwise False """
-    return issubclass(obj, a_class)
+    return issubclass(obj.__class__, a_class) and obj.__class__ != a_class
