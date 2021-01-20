9b4f183df8810a54017183a2e249534f82558d4d
TS-N <2501@holbertonschool.com>
2021-01-20 17:33:26 +0000

    add:10 create + pep8 + doc

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Method that defines a class Student """
+
+
+class Student:
+    """ A basic student class """
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retreives a dict of all the class attributes """
+        al = self.__dict__
+        if not attrs:
+            return al
+        dic = {}
+        for attr in attrs:
+            if attr in al:
+                dic[attr] = al[attr]
+        return dic
