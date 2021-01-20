6b2c51aa7268d9fe5a37a9cd1ccbb158f7b43931
TS-N <2501@holbertonschool.com>
2021-01-20 17:07:08 +0000

    add:0-9 + 15 create + pep8 + doc

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" 0. Read file """
+
+def read_file(filename=""):
+    """ reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, mode="r", encoding-"utf-8") as f:
+        print(f.read())
---
+++ 0x0B-python-input_output/read_write_heap.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" 15. Hack the VM """
+import sys
+import os
+
+
+def main():
+    """ finds a string in the heap of a running process, and replaces it """
+    if len(sys.argv) != 4:
+        raise TypeError("usage: read_write_heap.py pid 
+                search_string replace_string")
+    pid = sys.argv[1]
+    old_string = sys.argv[2]
+    new_string = sys.argv[3]
+    pad = len(old_string) - len(new_string)
+    if pad < 0:
+        pad = 0
+
+    with open("/proc/" + pid + "/maps", mode="r") as maps:
+        lines = maps.readlines()
+    for line in lines:
+        sp = line.split()
+        if '[heap]' in sp:
+            break
+    print("[*]Heap:\n\tAddress range: {}\n\t
+            Permissions: {}\nFull line: {}".format(sp[0], sp[1], sp))
+    addresses = sp[0].split('-')
+    start_add = int(addresses[0], 16)
+    end_add = int(addresses[1], 16)
+    with open("/proc/" + pid + "/mem", mode="rb+") as mem:
+        mem.seek(start_add)
+        rel_add = 0
+        while (rel_add <= 30):
+            try:
+                s = mem.read(len(old_string))
+                mem.seek(start_add + rel_add, 0)
+                if s == bytes(old_string, 'ascii'):
+                    print('replace_string found at address {}'
+                            .format(hex(start_add + rel_add)))
+                    mem.write(bytes(new_string, 'ascii'))
+                    for i in range(0, pad):
+                        mem.write(bytes('\0', 'ascii'))
+                    break
+            except Exception as e:
+                print(e)
+                pass
+            mem.seek(1, 1)
+            rel_add += 1
+
+if __name__ == "__main__":
+    main()
