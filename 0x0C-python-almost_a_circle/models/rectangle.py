fd37683e475f016d59f9d15e78049130d14cd4e9
TS-N <2501@holbertonschool.com>
2021-01-26 13:02:44 +0000

    more doc

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,6 +15,7 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes the default attributes of the Base object """
         super().__init__(id)
         self.width = width
         self.height = height
