3182d9b1424d3cccdcc47b842c285bdc02110842
TS-N <tanguy.sn@gmail.com>
2021-03-26 13:34:51 +0000

    ex:0 create + pep8

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+""" a script that lists all states from the database hbtn_0e_0_usa """
+
+
+def main():
+    """ main function """
+
+    if (len(sys.argv) == 4):
+        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3])
+        c = db.cursor()
+        c.execute("SELECT * FROM states ORDER BY id ASC")
+        rs = c.fetchall()
+        for r in rs:
+            print(r)
+
+
+if __name__ == "__main__":
+    main()
