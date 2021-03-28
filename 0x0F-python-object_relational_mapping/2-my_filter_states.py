179febe137cbfca0c8e0901dce2cf56abd767afd
TS-N <tanguy.sn@gmail.com>
2021-03-28 11:00:00 +0000

    fix:2 have to force using .format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,8 @@ def main():
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-              (sys.argv[4],))
+    c.execute("SELECT * FROM states \
+               WHERE name = '{:s}' ORDER BY id ASC".format(sys.argv[4]))
     rs = c.fetchall()
     for r in rs:
         print(r)
