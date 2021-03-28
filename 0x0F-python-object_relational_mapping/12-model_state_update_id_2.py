98d10d51d7cbb3364ea56e0d374b3c3fa71853d8
TS-N <tanguy.sn@gmail.com>
2021-03-28 18:00:40 +0000

    add:12 create + pep8

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""adds the State object Louisiana to the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    """main function """
+
+    dialect = 'mysql'
+    driver = 'mysqldb'
+    username = sys.argv[1]
+    password = sys.argv[2]
+    host = 'localhost'
+    port = '3306'
+    database = sys.argv[3]
+    engine = create_engine("{}+{}://{}:{}@{}:{}/{}".format(dialect,
+                                                           driver,
+                                                           username,
+                                                           password,
+                                                           host,
+                                                           port,
+                                                           database))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    obj = session.query(State).filter(State.id == 2).first()
+    obj.name = 'New Mexico'
+    session.commit()
+
+
+if __name__ == "__main__":
+    main()
