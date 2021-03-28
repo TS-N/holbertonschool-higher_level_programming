5b4b38510bc26687d6cd2721fe929eea87326ec9
TS-N <tanguy.sn@gmail.com>
2021-03-28 11:03:29 +0000

    fix:3 move comments

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 """a script that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa where name matches the argument
 injection safe"""
+import MySQLdb
+import sys
 
 
 def main():
